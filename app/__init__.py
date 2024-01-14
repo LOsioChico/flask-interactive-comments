222fca13fc3152620f2671e0bf49484010f51248
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-01-14 10:44:54 -0500

    feat: add default config and remove debug parameter in app.run

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template
 def create_app():
     app = Flask(__name__)
 
+    # Default config
+    app.debug = True
+
     @app.route('/')
     def index():
         return render_template('index.html')
