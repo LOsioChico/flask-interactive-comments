7056b2229b50756734d19b40bac5133fc43fcaff
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-01-14 23:16:04 -0500

    feat: Add database initialization and population

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from app.utils import populate_db
+from app.db.sqlalchemy import db
 
 
 def create_app():
@@ -6,9 +9,18 @@ def create_app():
 
     # Default config
     app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+    db.init_app(app)
 
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    with app.app_context():
+        # Create Database Models
+        db.create_all()
+        # Populate the database if empty
+        populate_db()
 
     return app
